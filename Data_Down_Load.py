91c112438616d9a05645abdc5ac2b95699a37954
kylin0322 <kylin0322@qq.com>
2023-06-17 14:08:14 +0800

    update

--- Data_Down_Load.py
+++ Data_Down_Load.py
@@ -1,34 +1,15 @@
 
-import os
-import sys
-
-PRODUCT_DIR = os.path.dirname(os.path.abspath(__file__))
-os.chdir(PRODUCT_DIR)
-sys.path.append(r"C:\FalconX")
-sys.path.append(PRODUCT_DIR)
-sys.path.append(os.path.join(PRODUCT_DIR, "scripts"))
-sys.path.append(os.path.join(PRODUCT_DIR, "scripts", "mfg-site-packages"))
-sys.path.append(os.path.join(PRODUCT_DIR, "scripts", "sctl-sol-yeti-scripts"))
-sys.path.append(os.path.join(PRODUCT_DIR, "scripts", "sctl-falcon"))
+import selenium_chrome
 
 
-from yeti_service_window import MainWindow
-from PySide6 import QtWidgets
-from qt_material import apply_stylesheet
-
-
-
-import conf
-import version
 
 def main(argv):
 
-
-    myapp = QtWidgets.QApplication(argv)
-    mainWin = MainWindow(conf.repair_rfu_dict, conf.audit_boid_dict, conf.product_names_dict, conf.fw_info_dict, conf.has_rtc_support_dict, conf.USB_PORTS, version=version.__version__)
-    apply_stylesheet(myapp, theme='dark_blue.xml')
-    mainWin.show()
-    myapp.exec()
+    web=webdriver_chrome()
+    web.init()
+     web.open_web()
+    
+  
 
 
 if __name__  == '__main__':
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Data_Down_Load
 Down load form web by pthon
+
+# Download_Defect
+
+
+
+
+
+
